4d88717c486420fbce4899e60ffaa041ec99e7c4
killshotroxs <krishnal@hotmail.com>
2023-08-28 13:12:08 +0530

    Testing

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ const toggleTheme = () => {
 darkThemeQuery.addEventListener("change", toggleTheme);
 
 toggleTheme();
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".box3", { opacity: 0, y: 100, delay: 1, duration: 1 });
+
+gsap.from(".box4 img", {
+  scrollTrigger: ".box4 img",
+  opacity: 0,
+  scrub: true,
+  x: 300,
+  // normalizeScroll: true,
+  duration: 3,
+  start: "top top", // when the top of the trigger hits the top of the viewport
+  end: "+=500",
+});
