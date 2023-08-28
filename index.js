e6c04ce6ed43c1345aa1bde6ee639bda524b5328
killshotroxs <krishnal@hotmail.com>
2023-08-28 13:50:16 +0530

    animation to all images

--- index.js
+++ index.js
@@ -16,16 +16,82 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".box3", { opacity: 0, y: 100, delay: 1, duration: 1 });
 
-gsap.from(".box4 img",{
-  opacity:0,
-  x:100,
-  duration:1,
-  scrollTrigger:{
-    trigger:".box4 .img",
-    // start:"top 80%",
-    // end:"top 30%",
-    scrub:2,
-    toggleActions:"restart none none none",
-    markers:true,
-  }
-})
+gsap.from(".box4 img", {
+  opacity: 0,
+  x: 300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".box4 .img",
+    start: "70% 20% ",
+    end: "top 80%",
+    scrub: 1,
+    toggleActions: "restart none none none",
+    // markers: {
+    //   startColor: "white",
+    //   endColor: "white",
+    //   fontSize: "18px",
+    //   fontWeight: "bold",
+    //   indent: 20,
+    // },
+  },
+});
+
+gsap.from(".box5 img", {
+  opacity: 0,
+  x: -300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".box5 .img",
+    start: "80% 20% ",
+    end: "top 80%",
+    scrub: 1,
+    toggleActions: "restart none none none",
+    // markers: {
+    //   startColor: "white",
+    //   endColor: "white",
+    //   fontSize: "18px",
+    //   fontWeight: "bold",
+    //   indent: 20,
+    // },
+  },
+});
+
+gsap.from(".box6 img", {
+  opacity: 0,
+  x: 300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".box6 .img",
+    start: "80% 20% ",
+    end: "top 80%",
+    scrub: 1,
+    toggleActions: "restart none none none",
+    // markers: {
+    //   startColor: "white",
+    //   endColor: "white",
+    //   fontSize: "18px",
+    //   fontWeight: "bold",
+    //   indent: 20,
+    // },
+  },
+});
+
+gsap.from(".box7 img", {
+  opacity: 0,
+  x: -300,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".box7 .img",
+    start: "80% 20% ",
+    end: "top 80%",
+    scrub: 1,
+    toggleActions: "restart none none none",
+    // markers: {
+    //   startColor: "white",
+    //   endColor: "white",
+    //   fontSize: "18px",
+    //   fontWeight: "bold",
+    //   indent: 20,
+    // },
+  },
+});
